5a8acfd9f82755a9f75f165dd826eb10d172b4f1
ssi02014 <ssi02014@naver.com>
2026-04-10 00:06:08 +0900

    feat: comments controller 추가

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { PostsModule } from '../posts/posts.module';
-import { CommentsController } from './comments.controller';
+import {
+  CommentsController,
+  PostCommentsController,
+} from './comments.controller';
 import { CommentsService } from './comments.service';
 import { CommonModule } from '../common/common.module';
 
 @Module({
   imports: [CommonModule, UsersModule, PostsModule],
-  controllers: [CommentsController],
+  controllers: [PostCommentsController, CommentsController],
   providers: [CommentsService],
 })
 export class CommentsModule {}
